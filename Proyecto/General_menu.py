9891ee04de78a569aafa15adeabc4ca46153f250
EleusisGitCourse <eleusispc@live.com.mx>
2022-10-25 11:48:51 -0500

    se crea un diccionario para guardar la informacion de las personas

--- Proyecto/General_menu.py
+++ Proyecto/General_menu.py
@@ -1,10 +1,10 @@
+import cofig
 from Classes.persona import Person
-from Classes.listado import listado
 print("1.- Agrega una nueva persona")
 print("2.- Eliminar a una persona")
 print("3.- Buscar a una perosna")
 option = input("Elige una de las opciones")
-
+listado_person = {}
 match option:
     case '1':
         print("Ingresa los datos de la persona")
@@ -14,7 +14,11 @@ match option:
         address = input("address : ")
         new_person = Person(name,last_name,age,address)
         print(new_person.get_generla_infomation())
-        my_list = listado(new_person)
-        my_list.get_listado_items()
+        cofig.items_counter+=1
+        listado_person[str(cofig.items_counter)] = new_person
+
+
+        print(listado_person[str(cofig.items_counter)])
+       
 
     
