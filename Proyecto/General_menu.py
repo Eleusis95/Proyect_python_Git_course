15ba108b5652b7e4a8234f12d4b5af26b3d08fa8
EleusisGitCourse <eleusispc@live.com.mx>
2022-10-25 12:54:48 -0500

    Se modifica la opcion 2 de mostrar listado de personas

--- Proyecto/General_menu.py
+++ Proyecto/General_menu.py
@@ -1,12 +1,13 @@
 import cofig
 from Classes.persona import Person
 option = 0
+listado_person = {}
 while option != '5':
     print("1.- Agrega una nueva persona")
     print("2.- Mostar listado de perosna")
     print("3.- Buscar a una perosna")
     option = input("Elige una de las opciones")
-    listado_person = {}
+    
 
     match option:
         case '1':
@@ -22,7 +23,7 @@ while option != '5':
         case '2':
             print("--Este es el listado actual de las perosnas agregadas--")
             for key,persona in listado_person.items():
-                print(f"{key} : {persona}")
+                print(f"{key} {str(persona)}")
 
        
 
