44c82328f5457fd1a302fa48d85bb949c741c439
EleusisGitCourse <eleusispc@live.com.mx>
2022-10-25 12:04:29 -0500

    se agrega opcion 2 para mostar lostado de personas

--- Proyecto/General_menu.py
+++ Proyecto/General_menu.py
@@ -1,24 +1,29 @@
 import cofig
 from Classes.persona import Person
-print("1.- Agrega una nueva persona")
-print("2.- Eliminar a una persona")
-print("3.- Buscar a una perosna")
-option = input("Elige una de las opciones")
-listado_person = {}
-match option:
-    case '1':
-        print("Ingresa los datos de la persona")
-        name = input("name : ")
-        last_name = input("last name : ")
-        age = input("age : ")
-        address = input("address : ")
-        new_person = Person(name,last_name,age,address)
-        print(new_person.get_generla_infomation())
-        cofig.items_counter+=1
-        listado_person[str(cofig.items_counter)] = new_person
+option = 0
+while option != '5':
+    print("1.- Agrega una nueva persona")
+    print("2.- Mostar listado de perosna")
+    print("3.- Buscar a una perosna")
+    option = input("Elige una de las opciones")
+    listado_person = {}
 
+    match option:
+        case '1':
+            print("Ingresa los datos de la persona")
+            name = input("name : ")
+            last_name = input("last name : ")
+            age = input("age : ")
+            address = input("address : ")
+            new_person = Person(name,last_name,age,address)
+            print(new_person.get_generla_infomation())
+            cofig.items_counter+=1
+            listado_person[str(cofig.items_counter)] = new_person
+        case '2':
+            print("--Este es el listado actual de las perosnas agregadas--")
+            for key,persona in listado_person.items():
+                print(f"{key} : {persona}")
 
-        print(listado_person[str(cofig.items_counter)])
        
 
     
