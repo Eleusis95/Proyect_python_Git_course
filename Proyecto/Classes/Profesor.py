4b50d1b423888ac4473834c08d6986054532a156
EleusisGitCourse <eleusispc@live.com.mx>
2022-10-25 14:49:02 -0500

    it has been add __str__ function for class Proferos

--- Proyecto/Classes/Profesor.py
+++ Proyecto/Classes/Profesor.py
@@ -1,6 +1,13 @@
 from persona import Person
 
 class Profesor(Person):
-    def __init__(self,name,last_name, age,address,matter):
+    def __init__(self,name,last_name, age,address,matter,school):
         self.matter = matter
-        Person.__init__(name,last_name, age,address)
+        self.school = school
+        Person.__init__(self,name,last_name, age,address)
+    def __str__(self):
+        return f"{Person.__str__(self)} and teachs the matter : {self.matter} at school {self.school}"
+
+myprofesor = Profesor(name="Pedro",last_name="Lopez",age=85,address="Alcalde #43",matter="History",school="Enrique díaz")
+
+print(myprofesor)
